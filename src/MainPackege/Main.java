4e6730b83fc4025da67e61407ac4575bd88a81fe
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-03-19 20:53:43 +0300

    CREATED Darya clever

---
+++ src/MainPackege/Main.java
@@ -0,0 +1,9 @@
+package MainPackege;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Darya четырёхлистный клевер");
+    }
+}
